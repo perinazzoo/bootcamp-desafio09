1d9b1c8844104256a322efec8b8d9e73f6fb7cc9
Gabriel Perinazzo <git@gabrielperinazzo.live>
2020-12-31 03:11:30 -0300

    feat(products): create products route

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -5,6 +5,16 @@ import CreateProductService from '@modules/products/services/CreateProductServic
 
 export default class ProductsController {
   public async create(req: Request, res: Response): Promise<Response> {
-    // TODO
+    const { name, price, quantity } = req.body;
+
+    const createProduct = container.resolve(CreateProductService);
+
+    const product = await createProduct.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return res.json(product);
   }
 }
