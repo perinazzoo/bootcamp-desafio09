97e027e8eddaf2da0633cbb5b7bea43c257a711a
Gabriel Perinazzo <git@gabrielperinazzo.live>
2020-12-31 23:44:12 -0300

    feat(orders): add find by id route

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -7,10 +7,27 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
   public async show(req: Request, res: Response): Promise<Response> {
-    // TODO
+    const { id } = req.params;
+
+    const findOrder = container.resolve(FindOrderService);
+
+    const order = await findOrder.execute({
+      id,
+    });
+
+    return res.json(order);
   }
 
   public async create(req: Request, res: Response): Promise<Response> {
-    // TODO
+    const { customer_id, products } = req.body;
+
+    const createOrder = container.resolve(CreateOrderService);
+
+    const order = createOrder.execute({
+      customer_id,
+      products,
+    });
+
+    return res.json(order);
   }
 }
